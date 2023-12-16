17f088b1c9db6316433bd95dbbb45d25767cb3db
Roger Lo <sobadrush@icloud.com>
2023-12-16 23:15:14 +0800

    【feature】完成 log4j2 Delete log檔案測試

--- src/main/java/com/nanshan/springboothelloworld/SpringBootCheckPortApplication.java
+++ src/main/java/com/nanshan/springboothelloworld/SpringBootCheckPortApplication.java
@@ -11,8 +11,10 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringBootCheckPortApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/nanshan/springboothelloworld/controller/HelloController.java
+++ src/main/java/com/nanshan/springboothelloworld/controller/HelloController.java
@@ -1,11 +1,14 @@
 package com.nanshan.springboothelloworld.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.IntStream;
 
 /**
  * @author RogerLo
@@ -15,14 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/helloController")
 public class HelloController {
 
-    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
+    // private final Logger logger = LoggerFactory.getLogger(HelloController.class);
+    private Logger logger = LogManager.getLogger(HelloController.class);
 
     /**
      * URL: http://localhost:8070/SpringBootHelloWorld/helloController/sayHello
      */
     @GetMapping(path = "/sayHello", produces = { MediaType.TEXT_PLAIN_VALUE })
+    @Scheduled(fixedDelay = 3000)
     public String sayHello() {
         logger.info("=== Say Hello ===");
+        IntStream.rangeClosed(1, 10000).forEach(i -> {
+            logger.info(">>> @Scheduled 觸發: {} - @@@測試@@@", i);
+        });
         return "Hello world!";
     }
 
